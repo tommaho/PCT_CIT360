e137c5e228d7bd5c29b8b0e326bbb1f0ce5ed530
tommaho <tom@tommaho.com>
2022-09-03 11:00:03 -0400

    Move to includes
    moving css / js out to includes

--- Assignment_1/scripts/assignment_1.js
+++ Assignment_1/scripts/assignment_1.js
@@ -253,7 +253,6 @@ function run() {
     //    avgRunTime + " milliseconds<br>";
 
 
-
     let sortedLongMap = new Map([...longResultsMap.entries()].sort((a,b) => a[1] - b[1]));
 
     console.log(sortedLongMap);
