ad7e918b3bcd572c4e1e87e49a77609fb561871a
tommaho <tom@tommaho.com>
2022-09-21 20:28:27 -0400

    cleanup js

--- Assignment_2/scripts/assignment_2.js
+++ Assignment_2/scripts/assignment_2.js
@@ -8,15 +8,12 @@ function reset() {
 }
 
 
-
-
-//find the 5 character wide window that is the most diverse
 /**
  * Looked for an n-gram generation algo for continuous text and found inspiration from:
  * https://stackoverflow.com/questions/40763960/data-structure-for-ngrams#40765350
  *
- * @param str
- * @param n
+ * @param str the string to look in
+ * @param n the width, in characters, of the entries
  */
 function getNGrams(str, n){
 
@@ -69,34 +66,20 @@ function sortNGrams(map){
     return new Map([...map.entries()].sort((a,b) => b[1] - a[1]));
 };
 
-function main(str, n){
-
-    //let result = getNGrams(str, n);
-    //let sortedResults = sortNGrams(result);
-
-    //console.log(sortedResults);
-
-    //for string xyz, the most diverse n element sequence is asdfasfd, with n distinct characters
-    //string and highlight??
-
-}
 
 /**
  * Loading the file will initiate algo execution.
  *
  * Adapted directly from https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
  */
-function loadFile() {
+function loadFileAndRun() {
 
     const [file] = document.querySelector('input[type=file]').files;
     const reader = new FileReader();
 
     reader.addEventListener("load", () => {
-        // this will then display a text file
 
         let parsedText = parse(reader.result)
-
-        //displayFilePreview(reader.result);
 
         let resultHTML = '';
         let fileHTML = ''
@@ -133,7 +116,7 @@ function loadFile() {
 
 /**
  * simply split the text. Moved to a function in case I wanted to do something
- * else here
+ * additional here
  * @param text
  * @returns {*}
  */
@@ -152,8 +135,6 @@ function highlightWinner (text, winner ){
 
     let highlighted = text.substring(0, winnerPos) + "<span class='entry'>" +
         winner + "</span>" + text.substring(winnerPos + winner.length)
-
-    console.log(highlighted);
 
     return highlighted;
 };
